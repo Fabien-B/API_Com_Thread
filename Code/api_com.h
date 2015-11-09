7a0f8f5cb477936ef57ee9ff29cd446f68070274
Polo <nicolas.dzieciol@gmail.com>
2015-11-09 10:18:27 +0100

    client api

--- Code/api_com.h
+++ Code/api_com.h
@@ -1,6 +1,22 @@
 #ifndef API_COM_H_INCLUDED
 #define API_COM_H_INCLUDED
+#include <pthread.h>
 
+typedef communication {
+    int client_id;
+    int operation;
+    void * contenu;
+    pthread_mutex_t * mutex;
+
+} communication;
+
+int initMsg();
+int aboMsg(communication *);
+int sendMsg(int my_id, int dest);
+int recvMsg(int my_id);
+int getNbMsg(int my_id);
+int desaboMsg(int my_id);
+int finMsg();
 
 
 #endif // API_COM_H_INCLUDED
