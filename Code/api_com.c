fdf5a96dea42c0e21ac902a3ac9406523becf605
Fabien-B <fabien.bonneval@gmail.com>
2015-11-10 09:46:14 +0100

    begin code

--- Code/api_com.c
+++ Code/api_com.c
@@ -1,8 +1,60 @@
 #include "types_private.h"
 #include "api_com.h"
 
+pthread_mutex_t * mutex_abo;
+communication * com_abo;
+
 void * gestionnaire(void * arg)
 {
-	
+
 }
+
+int initMsg()
+{
+    pthread_t thread_gest;
+    if (pthread_create(&idThread1, NULL, foncThread1, NULL)!=0){
+        printf("erreur creation thread1\n");
+        exit(1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
