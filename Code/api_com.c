aed2e72be9bec0cf9432d016746a50c99fdeff8c
Fabien-B <fabien.bonneval@gmail.com>
2015-11-11 11:00:58 +0100

    Fix bug due to synchronisation between gestionnaire and initMsg.

--- Code/api_com.c
+++ Code/api_com.c
@@ -12,21 +12,21 @@ pthread_mutex_t _mutex_abo = PTHREAD_MUTEX_INITIALIZER;     //mutex utilisé pou
 pthread_cond_t _client_signal = PTHREAD_COND_INITIALIZER;   //condition pour sortir le gestionnaire de sa torpeur
 communication * _com_abo;                                   //utilisé à l'abonnement
 pthread_t * _thread_gest = NULL;                            //est à NULL si le gestionnaire n'est pas lancé
-int _abo_traite = 1;                                        //est à 0 si un abonnement est en cours
+int _abo_traite = 0;                                        //est à 0 si un abonnement est en cours, ou le lancement du gestionnaire
 
 void * gestionnaire(void * arg)
 {
+    pthread_mutex_lock(&_mutex_abo);    //lock mutex pour ne rien faire tant qu'on est pas en croisière
+
     int nb_messageries = 0;
     messagerie tab[NB_ABO_MAX];     //crée les messageries
-
+    _abo_traite = 1;                //variable pour la vérification de la phase croisière
+    pthread_mutex_unlock(&_mutex_abo);
+    pthread_cond_wait(&_client_signal, &_mutex_abo);      //attente d'un signal (avec unlock automatique du mutex, puis lock à la sortie)
     while(1)
     {
-        //pthread_mutex_lock(&_mutex_abo);
-        pthread_cond_wait(&_client_signal, &_mutex_abo);      //attente d'un signal
-
         if(!_abo_traite)    //un abonnement à gérer !
         {
-            //pthread_mutex_lock(&_mutex_abo);  // fait automatiquement à la sortie du wait.
             int flag = SUCCESS;     //succes by default, modified if there is an error.
 
             if(nb_messageries<NB_ABO_MAX)
@@ -48,21 +48,20 @@ void * gestionnaire(void * arg)
             {
                 flag = MAX_ABO;
             }
-
             pthread_mutex_lock(_com_abo->mutex);  //lock mutex client
             _com_abo->retour = flag;       //met la valeur de retour à success.
             pthread_cond_signal(_com_abo->signal_gestionnaire);   //réveille le client.
             pthread_mutex_unlock(_com_abo->mutex);    //unlock mutex client
-
-            _abo_traite = 1;    //abo traitée, on peut en faire d'autre !
-            pthread_mutex_unlock(&_mutex_abo); //on rend la main.
+            _abo_traite = 1;    //demande d'abo traitée, on peut en faire d'autre !
         }
 
         int i;
-        for(i=0;i<nb_messageries;i++) //parcours liste abo pour vérif id demandé.
+        for(i=0;i<nb_messageries;i++) //parcours liste abo executer les actions id demandées.
         {
             //ACTIONS (switch case)!
         }
+
+        pthread_cond_wait(&_client_signal, &_mutex_abo);      //attente d'un signal pour effectuer la boule suivante
     }
     pthread_exit(0);
 }
@@ -82,6 +81,12 @@ int initMsg()
     if (pthread_create(_thread_gest, NULL, gestionnaire, NULL)!=0){
         printf("erreur creation thread gestionnaire\n");
         return INIT_ERROR; //TECH_ERROR
+    }
+
+    while(_abo_traite == 0)     //si on arrive vite, on attend que le gestionnaire soit en régime de croisière.
+    {
+        pthread_mutex_lock(&_mutex_abo);    //le challenge : pouvoir prendre le mutex, et que _abo_traite soit à 1.
+        pthread_mutex_unlock(&_mutex_abo);
     }
     return SUCCESS;
 }
