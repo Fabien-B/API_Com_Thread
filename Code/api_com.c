d6bb59b6cb1637095eec4d48a0a83a81b9d40ebd
Fabien-B <fabien.bonneval@gmail.com>
2015-11-11 01:51:10 +0100

    Modify aboMsg.

--- Code/api_com.c
+++ Code/api_com.c
@@ -140,27 +140,44 @@ int finMsg()
 
 int aboMsg(communication * my_com)
 {
-	
-	&(my_com.signal_gestionnaire) = PTHREAD_COND_INITIALIZER;
-	&(my_com.mutex) = PTHREAD_MUTEX_INITIALIZER;
-	
-	
-	int abo_possible = 0;
-	while(abo_possible == 0)						// Si l'ancienne demande d'abonnement n'est pas prise en compte ont attend
-		{
-			pthread_mutex_lock(&_mutex_abo);
-			if(_abo_traite == 1)
-			{
-				abo_possible = 1;
-				_abo_traite = 0;
-				//abonnement
-			}
-			
-			pthread_mutex_unlock(&_mutex_abo);
-			if(!abo_possible)
-			{
-				usleep(1000);
-			}
-		}
-	
+    pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+    my_com->signal_gestionnaire = &cond;
+	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    my_com->mutex = &mutex;
+    my_com->operation = ABO;
+    my_com->retour = -1;
+
+	int abo_ok = 0;
+
+	while(abo_ok == 0)						// Si l'ancienne demande d'abonnement n'est pas prise en compte ont attend
+    {
+        pthread_mutex_lock(&_mutex_abo);
+
+        if(_abo_traite == 1)    //pas d'abonnement en cours, on y va !
+        {
+            abo_ok = 1;         //on peut s'abonner -> pas besoin de refaire la boucle.
+            _abo_traite = 0;    //indique un abonnement en cours. le gestionnaire le remettra a 1.
+
+            //abonnement
+            _com_abo = my_com; //mise à dispo de ma struct communication.
+
+            pthread_cond_signal(&_client_signal);   //envoie signal pour le gestionnaire
+
+            while(my_com->retour == -1)              //attente d'un signal venant du gestionnaire
+            {
+                pthread_cond_wait(my_com->signal_gestionnaire, my_com->mutex);   //il communique avec les objets que je lui ai spécifiés.
+            }
+
+            return my_com->retour;                   //retourne le code renvoyé par le gestionnaire
+
+        }
+
+        pthread_mutex_unlock(&_mutex_abo);
+        if(!abo_ok)
+        {
+            usleep(1000);
+        }
+    }
+
+    return TECH_ERROR;
 }
