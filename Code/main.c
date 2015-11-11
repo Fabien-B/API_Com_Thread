6a525aff8d7e8ee1273989fa075c0eb8b511a73f
Fabien-B <fabien.bonneval@gmail.com>
2015-11-11 02:57:26 +0100

    Basic test code.

--- Code/main.c
+++ Code/main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "api_com.h"
+#include <unistd.h>
+#include <pthread.h>
 
 int main()
 {
-
+    int ret;
     printf("Hello world!\n");
+
+
+    ret = initMsg();
+    printf("Valeur de retour init: %d\n",ret);
+//usleep(1000);
+    communication moi;
+    moi.client_id = 2;
+    ret = aboMsg(&moi);
+    printf("Valeur de retour abo: %d\n",ret);
+
+
+    communication moi2;
+    moi2.client_id = 2;
+    ret = aboMsg(&moi2);
+    printf("Valeur de retour abo: %d\n",ret);
+
+
+
+    ret = finMsg();
+    printf("Valeur de retour fin: %d\n",ret);
     return 0;
 }
