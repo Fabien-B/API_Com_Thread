96a580ee0e60307296e883a2a835c8b4b9752877
Fabien-B <fabien.bonneval@gmail.com>
2015-11-12 06:26:01 +0100

    Abo.

--- Code/abo_handeling.c
+++ Code/abo_handeling.c
@@ -18,7 +18,7 @@ int aboMsg(communication * my_com)
     my_com->signal_gestionnaire = &cond;
 	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
     my_com->mutex = &mutex;
-    my_com->operation = ABO;
+    my_com->operation = NO_OP;
     my_com->retour = -1;
 
 	int abo_ok = 0;
@@ -33,14 +33,14 @@ int aboMsg(communication * my_com)
             //abonnement
             _com_abo = my_com; //mise à dispo de ma struct communication.
 
+            pthread_mutex_lock(my_com->mutex);      //lock pour recevoir le signal pendant le wait (pas avant)
             pthread_cond_signal(&_client_signal);   //envoie signal pour le gestionnaire
             pthread_mutex_unlock(&_mutex_abo);      //et libération mutex
-
             while(my_com->retour == -1)              //attente d'un signal venant du gestionnaire
             {
                 pthread_cond_wait(my_com->signal_gestionnaire, my_com->mutex);   //il communique avec les objets que je lui ai spécifiés.
             }
-
+            pthread_mutex_unlock(my_com->mutex);
             return my_com->retour;                   //retourne le code renvoyé par le gestionnaire
 
         }
@@ -57,10 +57,25 @@ int aboMsg(communication * my_com)
 
 
 
-int desaboMsg(communication * my_com)
+int desaboMsg(communication * mycom)
 {
+    if(_thread_gest==NULL)
+    {
+        return NO_SERVICE;
+    }
 
+    mycom->operation = DESABO;
+    mycom->retour = -1;
 
+    pthread_mutex_lock(&_mutex_abo);
+    pthread_cond_signal(&_client_signal);   //envoie signal pour le gestionnaire
+    pthread_mutex_unlock(&_mutex_abo);
 
+    while(mycom->retour == -1)
+    {
+        pthread_cond_wait(mycom->signal_gestionnaire, mycom->mutex);
+    }
 
+    mycom->operation = NO_OP;
+    return mycom->retour;
 }
