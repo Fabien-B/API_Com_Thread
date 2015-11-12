bab1212b73f3de99166db7c13ade41d6f848e44e
Fabien-B <fabien.bonneval@gmail.com>
2015-11-12 06:26:22 +0100

    gestionnaire.

--- Code/gestionnaire.c
+++ Code/gestionnaire.c
@@ -20,11 +20,12 @@ void * gestionnaire(void * arg)
 
     int nb_messageries = 0;
     messagerie tab[NB_ABO_MAX];     //crée les messageries
+
     _abo_traite = 1;                //variable pour la vérification de la phase croisière
-    pthread_mutex_unlock(&_mutex_abo);
     pthread_cond_wait(&_client_signal, &_mutex_abo);      //attente d'un signal (avec unlock automatique du mutex, puis lock à la sortie)
     while(1)
     {
+        //_mutex_abo locké par le wait
         if(!_abo_traite)    //un abonnement à gérer !
         {
             int flag = SUCCESS;     //succes by default, modified if there is an error.
@@ -34,15 +35,21 @@ void * gestionnaire(void * arg)
                 int i;
                 for(i=0;i<nb_messageries;i++) //parcours liste abo pour vérif id demandé.
                 {
+                    pthread_mutex_lock(tab[i].client->mutex);
+                    pthread_mutex_lock(????????????????????????|||||????);
                     if(tab[i].client->client_id == _com_abo->client_id)
                     {
                         flag = ID_IN_USE;
                     }
+                    pthread_mutex_unlock(tab[i].client->mutex);
                 }
 
-                tab[nb_messageries].client = _com_abo;  //tout ok, stockage dans le tableau
-                tab[nb_messageries].first_msg = NULL;   //pas de msg au début
-                nb_messageries ++;  //un abo de plus.
+                if(flag != ID_IN_USE)   //si id dispo, on enregistre.
+                {
+                    tab[nb_messageries].client = _com_abo;  //tout ok, stockage dans le tableau
+                    tab[nb_messageries].first_letter = NULL;   //pas de msg au début
+                    nb_messageries ++;  //un abo de plus.
+                }
             }
             else
             {
@@ -53,12 +60,42 @@ void * gestionnaire(void * arg)
             pthread_cond_signal(_com_abo->signal_gestionnaire);   //réveille le client.
             pthread_mutex_unlock(_com_abo->mutex);    //unlock mutex client
             _abo_traite = 1;    //demande d'abo traitée, on peut en faire d'autre !
+            ///pthread_mutex_unlock(&_mutex_abo);
         }
+
 
         int i;
         for(i=0;i<nb_messageries;i++) //parcours liste abo executer les actions id demandées.
         {
-            //ACTIONS (switch case)!
+//usleep(1000);
+//printf("NB MSGR : %d (%d)\n", nb_messageries,i);
+printf("avant lock %d\n",i);
+            pthread_mutex_lock(tab[i].client->mutex);
+printf("APRES lock %d\n",i);
+            if(tab[i].client->operation != NO_OP)
+            {
+                //ACTIONS (switch case)!
+                //int ret;
+                int ret = -5;
+                /*switch(tab[i].client->operation)
+                {
+                    case SENDMSG:
+                        ret = handleSend(tab[i].client->client_id, tab[i].client->contenu);
+                        break;
+                    case RECVMSG:
+                        //ret = handleRcv();
+                        break;
+
+
+
+
+                }*/
+                tab[i].client->retour = ret;
+                pthread_cond_signal(tab[i].client->signal_gestionnaire);
+                //printf("tggggggg\n");
+            }
+            pthread_mutex_unlock(tab[i].client->mutex);
+printf("ertre\n");
         }
 
         pthread_cond_wait(&_client_signal, &_mutex_abo);      //attente d'un signal pour effectuer la boule suivante
@@ -66,3 +103,15 @@ void * gestionnaire(void * arg)
     pthread_exit(0);
 }
 
+
+
+int handleSend(int sender_id, void * contenu)   //crée un message daté et signé avec les données du client.
+{
+    printf("ON ENVOIE!!!\n");
+    return 0;
+}
+
+void handleRcv()
+{
+
+}
