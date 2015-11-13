81870e5bee50b30726cdebfcf6a47c8d158dda23
Fabien-B <fabien.bonneval@gmail.com>
2015-11-13 10:13:14 +0100

    End : new method.

--- Code/init_end_service.c
+++ Code/init_end_service.c
@@ -32,58 +32,46 @@ int initMsg()
 
 
 
-int finMsg()
+int finMsg(int force)
 {
     if(_thread_gest==NULL)  //thread non lancé, pas besoinde l'arreter.
     {
         return NO_SERVICE;
     }
 
-    ///Creation struct communication et remplissage des champs.
-    communication my_com;
-    my_com.client_id = -1;
-    pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-    my_com.signal_gestionnaire = &cond;
-	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-    my_com.mutex = &mutex;
-    my_com.operation = CLOSESERVICE;
-    my_com.retour = -1;
+    communication fin_com;
+    fin_com.client_id = -99;
 
-	int abo_ok = 0;
+    pthread_mutex_lock(&_mutex_abo);                                                         //sans doute inutile
+    fin = 1;
+    pthread_mutex_unlock(&_mutex_abo);
+                                                          //sans doute inutile
+    aboMsg(&fin_com);
 
-	while(abo_ok == 0)						// Si l'ancienne demande d'abonnement n'est pas prise en compte ont attend
+    if(force)
     {
-        pthread_mutex_lock(&_mutex_abo);
-
-        if(_abo_traite == 1)    //pas d'abonnement en cours, on y va !
-        {
-            abo_ok = 1;         //on peut s'abonner -> pas besoin de refaire la boucle.
-            _abo_traite = 0;    //indique un abonnement en cours. le gestionnaire le remettra a 1.
-
-            //abonnement
-            _com_abo = &my_com; //mise à dispo de ma struct communication.
-printf("stop\n");
-sleep(2);
-            pthread_cond_signal(&_client_signal);   //envoie signal pour le gestionnaire
-            pthread_mutex_unlock(&_mutex_abo);
-            //int ret = my_com.retour;
-            while(_com_abo->retour == -1)              //attente d'un signal venant du gestionnaire
-            {
-                pthread_cond_wait(_com_abo->signal_gestionnaire, _com_abo->mutex);   //il communique avec les objets que je lui ai spécifiés.
-            }
-
-            pthread_mutex_unlock(&_mutex_abo);
-            int ret = _com_abo->retour;
-            return ret;                   //retourne le code renvoyé par le gestionnaire
-
-        }
-
-        pthread_mutex_unlock(&_mutex_abo);
-        if(!abo_ok)
-        {
-            usleep(1000);
-        }
+        fin_com.operation = CLOSESERVICE/*_FORCED*/;
+    }
+    else
+    {
+        fin_com.operation = CLOSESERVICE;
     }
 
-    return TECH_ERROR;
+
+    fin_com.retour = -1;
+
+    pthread_mutex_lock(&_mutex_abo);
+    pthread_mutex_lock(fin_com.mutex);  //je lock mon mutex pour que le signal ne me soit envoyé que pendant mon wait
+    pthread_cond_signal(&_client_signal);   //envoie signal pour le gestionnaire
+    pthread_mutex_unlock(&_mutex_abo);      //et on le laisse bosser
+    while(fin_com.retour == -1)              //tant que le gestionnaire n'a pas fait son action...
+    {
+        pthread_cond_wait(fin_com.signal_gestionnaire, fin_com.mutex);    //on attend
+    }
+
+    int code_retour = fin_com.retour;
+    fin_com.operation = NO_OP;
+    pthread_mutex_unlock(fin_com.mutex);
+
+    return code_retour;
 }
