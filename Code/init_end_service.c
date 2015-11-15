96acc903fba5a732cf923a3bcd3e11e1ae22d1e4
Fabien-B <fabien.bonneval@gmail.com>
2015-11-15 02:01:45 +0100

    remove useless mutex lock.

--- Code/init_end_service.c
+++ Code/init_end_service.c
@@ -1,3 +1,4 @@
+#define _REENTRANT
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -16,15 +17,21 @@ int initMsg()
     pthread_t thread_gestionnaire;          //on déclare un pthread_t
     _thread_gest = &thread_gestionnaire;    //et on le donne a notre variable globale
 
-    if (pthread_create(_thread_gest, NULL, gestionnaire, NULL)!=0){
+    pthread_mutex_lock(&_mutex_abo);
+
+    //if (pthread_create(_thread_gest, NULL, gestionnaire, NULL)!=0){
+    if (pthread_create(_thread_gest, PTHREAD_CREATE_JOINABLE, gestionnaire, NULL)!=0){
         printf("erreur creation thread gestionnaire\n");
         return INIT_ERROR; //TECH_ERROR
     }
+    pthread_mutex_unlock(&_mutex_abo);
 
     while(_abo_traite == 0)     //si on arrive vite, on attend que le gestionnaire soit en régime de croisière.
     {
-        pthread_mutex_lock(&_mutex_abo);    //le challenge : pouvoir prendre le mutex, et que _abo_traite soit à 1.
-        pthread_mutex_unlock(&_mutex_abo);
+        //usleep(1000);
+        pthread_mutex_lock(&_mutex_abo);
+        pthread_mutex_unlock(&_mutex_abo);    //le challenge : pouvoir prendre le mutex, et que _abo_traite soit à 1.
+
     }
     return SUCCESS;
 }
@@ -40,38 +47,45 @@ int finMsg(int force)
     }
 
     communication fin_com;
-    fin_com.client_id = -99;
 
-    pthread_mutex_lock(&_mutex_abo);                                                         //sans doute inutile
+    pthread_mutex_lock(&_mutex_abo);
     fin = 1;
     pthread_mutex_unlock(&_mutex_abo);
-                                                          //sans doute inutile
-    aboMsg(&fin_com);
 
+    aboMsg(&fin_com, 45);
+
+    pthread_mutex_lock(fin_com.mutex);
+
+//sleep(1);
     if(force)
     {
-        fin_com.operation = CLOSESERVICE/*_FORCED*/;
+        fin_com.operation = CLOSESERVICE_FORCED;
     }
     else
     {
         fin_com.operation = CLOSESERVICE;
     }
 
-
     fin_com.retour = -1;
 
-    pthread_mutex_lock(&_mutex_abo);
-    pthread_mutex_lock(fin_com.mutex);  //je lock mon mutex pour que le signal ne me soit envoyé que pendant mon wait
     pthread_cond_signal(&_client_signal);   //envoie signal pour le gestionnaire
-    pthread_mutex_unlock(&_mutex_abo);      //et on le laisse bosser
+//sleep(2);
+
     while(fin_com.retour == -1)              //tant que le gestionnaire n'a pas fait son action...
+    //while(1)              //tant que le gestionnaire n'a pas fait son action...
     {
+    usleep(1000);
         pthread_cond_wait(fin_com.signal_gestionnaire, fin_com.mutex);    //on attend
     }
 
     int code_retour = fin_com.retour;
     fin_com.operation = NO_OP;
     pthread_mutex_unlock(fin_com.mutex);
-
+    //pthread_join(*_thread_gest,NULL);
+    //int pj;
+   /* if(pthread_join(*_thread_gest,NULL))
+    {
+        return pj;
+    }*/
     return code_retour;
 }
