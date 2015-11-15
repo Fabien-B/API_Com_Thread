c2e812d317ac78390a5a5f5fdf509b6b1323d35d
Fabien-B <fabien.bonneval@gmail.com>
2015-11-16 00:39:38 +0100

    Test: write some test code.

--- Code/main.c
+++ Code/main.c
@@ -15,15 +15,39 @@ int main()
 
     communication com1;
     ret = aboMsg(&com1,5);
-    printf("ttttttt: %d\n",ret);
+    printf("abo %d: %d\n",com1.client_id,ret);
 
     communication com2;
-    ret = aboMsg(&com2,6);
-    printf("------: %d\n",ret);
+    ret = aboMsg(&com2,51);
+    printf("abo %d: %d\n",com2.client_id,ret);
 
+    communication com3;
+    ret = aboMsg(&com3,15);
+    printf("abo %d: %d\n",com3.client_id,ret);
 
-    int a = 123;
-    sendMsg(&com1,5,&a);
+    //envoie d'un int
+    int a = 42;
+    ret = sendMsg(&com1,15,&a, sizeof(a));
+    printf("send : %d\n", ret);
+
+    //envoie d'une chaine de caractères
+    char b[50] = "Si tu vois ce message, c'est gagne !";
+    ret = sendMsg(&com2,15,&b, 50*sizeof(char));
+    printf("send : %d\n", ret);
+
+    //reception d'un int
+    message * mymess;
+    ret = recvMsg(&com3,&mymess);
+    printf("recv : %d\n", ret);
+    int* aq = mymess->contenu;
+    printf("message: %d\n",*aq);
+
+    //reception d'une chaine de caractères
+    message * mymess2;
+    ret = recvMsg(&com3,&mymess2);
+    printf("recv : %d\n", ret);
+    char * chaine = mymess2->contenu;
+    printf("message: %s\n",chaine);
 
     ret = finMsg();
     printf("Valeur de retour fin: %d\n",ret);
